e5aee706d28b67afb90ebda2e1b4690b82f1d1eb
Hiradoras <forwoermind@gmail.com>
2022-03-14 23:43:25 +0600

    now I can pass posts from db to home. html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from msilib.schema import ListView
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from .models import Post
 
