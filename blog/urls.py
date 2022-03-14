a3b0b9e4d945c99be3226e2c0a0f2b5b267a14d4
Hiradoras <forwoermind@gmail.com>
2022-03-15 00:09:36 +0600

    post detail added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from blog.models import Post
-from .views import HomeView
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('<int:pk>/detail', PostDetailView.as_view(), name='post-detail' )
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from msilib.schema import ListView
+from re import template
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class HomeView(ListView):
     model = Post
     template_name = 'blog/home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name  = 'blog/post_detail.html'
