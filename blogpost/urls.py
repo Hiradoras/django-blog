423bc3d1974f7ae680c76c781151cd057edb7571
Hiradoras <forwoermind@gmail.com>
2022-03-31 17:25:42 +0600

    addd favicon

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('blogpost/images/favicon.ico'))),
     path('', include('blog.urls')),
     path('members/', include('register.urls')),
     
